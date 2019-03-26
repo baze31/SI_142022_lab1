30aa387cd7d9eba6a403ca2310c26d306cf3dea4
baze31 <37380402+baze31@users.noreply.github.com>
2019-03-26 16:24:40 +0100

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void printMyName () {
+	
+	cout "Blagoj Dimitrov"
+}
+
+void printMyIndex () {
+	cout "142022"
+}
+
+int main () {
+	printMyIndex()
+	printMyName()
+}
